0ec0ce93f2f06f1c326c3479c8080e2fbda9a884
lea zaoui <lea.myriam.zaoui@gmail.com>
2020-06-17 10:18:12 +0200

    New feature : add models

---
+++ images/single-recettes.php
@@ -0,0 +1,55 @@
+ <?php
+/**
+ * The template for displaying all pages, single posts and attachments
+ *
+ * This is a new template file that WordPress introduced in
+ * version 4.3.
+ *
+ * @package OceanWP WordPress theme
+ */
+
+get_header(); ?>
+
+	<?php do_action( 'ocean_before_content_wrap' ); ?>
+
+	<div id="content-wrap" class="container clr">
+
+		<?php do_action( 'ocean_before_primary' ); ?>
+    <h1>Ici content-recettes.php</h1>
+
+		<div id="primary" class="content-area clr">
+
+			<?php do_action( 'ocean_before_content' ); ?>
+
+			<div id="content" class="site-content clr">
+
+				<?php do_action( 'ocean_before_content_inner' ); ?>
+
+				<?php
+				// Elementor `single` location
+				if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'single' ) ) {
+
+					// Start loop
+					while ( have_posts() ) : the_post();
+
+		    		get_template_part( 'partials/single/content-recettes', get_post_type() );
+
+					endwhile;
+
+				} ?>
+
+				<?php do_action( 'ocean_after_content_inner' ); ?>
+
+			</div><!-- #content -->
+
+			<?php do_action( 'ocean_after_content' ); ?>
+
+		</div><!-- #primary -->
+
+		<?php do_action( 'ocean_after_primary' ); ?>
+
+	</div><!-- #content-wrap -->
+
+	<?php do_action( 'ocean_after_content_wrap' ); ?>
+
+<?php get_footer(); ?>
